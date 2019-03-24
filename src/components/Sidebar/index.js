7445027a1212b3c430acc88ce3c31884f4f3f703
Stephanie Giang <stephaniegiang1@gmail.com>
2019-03-23 21:19:27 -0400

    Change links and add states

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import Collapsible from 'react-collapsible'
 
 import './sidebar.scss'
 
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      add: false,
+      delete: false,
+      swap: false,
+    }
+
+    this.addClicked = this.addClicked.bind(this)
+    this.deleteClicked = this.deleteClicked.bind(this)
+    this.swapClicked = this.swapClicked.bind(this)
+  }
   searchCourse() {
     //need to check all the inputs and make sure at least 1 is not empty
   }
@@ -31,91 +42,83 @@ class Sidebar extends React.Component {
     document.getElementById('semester').innerHTML = 'Fall 2020'
   }
 
+  addClicked() {
+    this.setState({
+      add: true,
+      delete: false,
+      swap: false,
+    })
+  }
+
+  deleteClicked() {
+    this.setState({
+      add: false,
+      delete: true,
+      swap: false,
+    })
+  }
+
+  swapClicked() {
+    this.setState({
+      add: false,
+      delete: false,
+      swap: true,
+    })
+  }
+
   render() {
     return (
-      <div>
-        <div class="semester-section">
-          <h1>
-            <label id="semester" value="Fall 2019">
-              Fall 2019
-            </label>
-          </h1>
-          <div className="dropdown">
-            <span class="change-semester">Change Semester</span>
-            <div className="dropdown-content">
-              <p id="fall2019" onClick={this.myFunction}>
+      <div className="side-container">
+        <div className="sidebar">
+          <div className="semester-section">
+            <h1>
+              <label id="semester" value="Fall 2019">
                 Fall 2019
-              </p>
-              <p id="winter2019" onClick={this.myFunction1}>
-                Winter 2019
-              </p>
-              <p id="summer2019" onClick={this.myFunction2}>
-                Summer 2019
-              </p>
-              <p id="fall2020" onClick={this.myFunction3}>
-                Fall 2020
-              </p>
+              </label>
+            </h1>
+            <div className="dropdown">
+              <span className="change-semester">Change Semester</span>
+              <div className="dropdown-content">
+                <p id="fall2019" onClick={this.myFunction}>
+                  Fall 2019
+                </p>
+                <p id="winter2019" onClick={this.myFunction1}>
+                  Winter 2019
+                </p>
+                <p id="summer2019" onClick={this.myFunction2}>
+                  Summer 2019
+                </p>
+                <p id="fall2020" onClick={this.myFunction3}>
+                  Fall 2020
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <Collapsible
-          trigger="+  Add Class"
-          onOpening={this.props.toggle}
-          onClosing={this.props.toggle}
-        >
-          <div>Fill in 1 of the follwing sections to search</div>
-          <Collapsible trigger="Course Code" onClose={this.resetCourseCode}>
-            <label>Enter the Course Code</label>
-            <input type="text" placeholder="Course Code" id="courseCode" />
-          </Collapsible>
-          <Collapsible trigger="Course Name" onClose={this.resetCourseName}>
-            <label>Enter the Course Name</label>
-            <input type="text" placeholder="Course Name" id="courseName" />
-          </Collapsible>
-          <Collapsible trigger="Department/Subject">
-            <p>
-              <label>Filter one of the following</label>
-            </p>
-            <p>
-              <label> Department</label>{' '}
-              <select>
-                <option value="null" />
-                <option value="Engineering">Engineering</option>
-                <option value="Telfer">Telfer</option>
-                <option value="Arts">Arts</option>
-                <option value="Law">Law</option>
-              </select>
-            </p>
-
-            <p>
-              <label> Subject</label>{' '}
-              <select>
-                <option value="AIR">AIR</option>
-                <option value="SEG">SEG</option>
-                <option value="HOM">HOM</option>
-                <option value="FOO">FOO</option>
-              </select>
-            </p>
-
-            <p>
-              <label> Level</label>{' '}
-              <select>
-                <option value="Undergraduate">Undergraduate</option>
-                <option value="Graduate">Graduate</option>
-                <option value="PHD">PHD</option>
-              </select>
-            </p>
-          </Collapsible>
-          <button type="button" onClick={this.searchCourse}>
-            Search
-          </button>
-        </Collapsible>
-        <Collapsible trigger="Delete Class">
-          <div />
-        </Collapsible>
-        <div className="sidebar">
-          Hello im the sidebar Hello im the sidebar Hello im the sidebar Hello
-          im the sidebar Hello im the sidebar
+          <div className="menu">
+            <div className="add-class">
+              <button
+                id="add"
+                className={this.state.add ? 'active' : ''}
+                onClick={this.addClicked}
+              >
+                <i className="fas fa-plus" /> Add Class
+              </button>
+            </div>
+            <button
+              id="drop"
+              className={this.state.delete ? 'active' : ''}
+              onClick={this.deleteClicked}
+            >
+              <i className="fas fa-trash-alt" /> Drop Class
+            </button>
+            <button
+              id="swap"
+              className={this.state.swap ? 'active' : ''}
+              onClick={this.swapClicked}
+            >
+              <i className="fas fa-exchange-alt" /> Swap Class
+            </button>
+          </div>
         </div>
       </div>
     )
