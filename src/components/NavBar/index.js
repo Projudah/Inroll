4e367ebcf81a06866f9a38716af4de2d7e9fe2b6
Stephanie Giang <stephaniegiang1@gmail.com>
2019-03-23 22:34:06 -0400

    Add logo

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,11 +7,14 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <div className="title">InRoll</div>
+        <div className="title"><i class="fas fa-graduation-cap"></i>InRoll</div>
         <div className="search">
           <SearchBar
             toggleSearchClassModal={this.props.toggleSearchClassModal}
           />
+        </div>
+        <div className="help-page">
+          <i class="far fa-question-circle"></i>
         </div>
       </div>
     )
