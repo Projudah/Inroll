883fcceded28a631b99c5298a76ca44386e95253
Projudah <projudah@gmail.com>
2019-03-29 16:09:03 -0400

    fixed overlapping buttons

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -10,28 +10,34 @@ class Progress extends Component {
 
 	render() {
 		return <div className = "progress_div">
-		<div className = "buttons">
+			<div className = "buttons">
+			<div className = "leftContain">
 
-		<PointButton
-		name ="Cancel"
-		direction="left"
-		click={this.cancel}/>
+				<PointButton
+				name ="Cancel"
+				direction="left"
+				click={this.cancel}/>
+			</div>
 
-		<Stepper steps={ [{title: 'Select Classes'}, {title: 'View Selection'}, {title: 'Confirm'}] }
-		activeStep={this.props.step}
-		activeColor = "#bb1a35"
-		completeColor = "#242424"
-		circleTop = {30}
-		size = {32}
-		circleFontSize = {15}
-		/>
-		<PointButton
-		name ="Next"
-		direction="right"
-		click={this.next}/>
+				<Stepper steps={ [{title: 'Select Classes'}, {title: 'View Selection'}, {title: 'Confirm'}] }
+				activeStep={this.props.step}
+				activeColor = "#bb1a35"
+				completeColor = "#242424"
+				circleTop = {30}
+				size = {32}
+				circleFontSize = {15}
+				lineMarginOffset = {4}
+				defaultBorderWidth = {0}
+				/>
+			<div className = "rightContain">
+				<PointButton
+				name ="Next"
+				direction="right"
+				click={this.next}/>
+			</div>
 
 
-		</div>
+			</div>
 		</div>;
 	}
 }
